3479f157ec6394897b96e2ecff1ee1b1931e6429
trizpasser <90735264+trizpasser@users.noreply.github.com>
2022-10-20 21:19:32 -0300

    Create 2_3.go

---
+++ 2_3.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+
+func main() {
+    var j int
+    
+    fmt.Print("Insira um numero: ")
+    fmt.Scan(&j)
+   
+    for i := 0; i <= j; i++ {
+	    fmt.Println("Contador: ", i)
+    }
+}
